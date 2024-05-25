bd90ce4f2bc8f8231be8ddf2e418863e1c01571f
trifonova_o_e <myowl@list.ru>
2024-05-25 23:39:30 +0300

    Начальная версия

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        FamilyTree familyTree = new FamilyTree();
+        Member tanya = new Member("Tanya", "Kruglova", Sex.Woman, LocalDate.of(1972,7,11));
+        Member sergey = new Member("Sergey", "Khrisanov", Sex.Man, LocalDate.of(1962,1,27));
+
+        familyTree.addMember(tanya);
+        familyTree.addMember(sergey);
     }
 }
--- src/Member.java
+++ src/Member.java
@@ -16,23 +16,23 @@ public class Member {
     public Member(String name, String surname, Sex sex, LocalDate birth, LocalDate death, Member child,
                    Member father, Member mother) {
         id = -1;
-        this.name = name;
-        this.surname = surname;
-        this.sex = sex;
-        this.dateOfBirth = birth;
-        this.dateOfDeath = death;
+        this.setName(name);
+        this.setSurname(surname);
+        this.setSex(sex);
+        this.setDateOfBirth(birth);
+        this.setDateOfDeath(death);
 
-        children = new ArrayList<>();
+        setChildren(new ArrayList<>());
         if (child != null) {
-            children.add(child);
+            getChildren().add(child);
         }
 
-        parents = new ArrayList<>();
+        setParents(new ArrayList<>());
         if (father != null) {
-            parents.add(father);
+            getParents().add(father);
         }
         if (mother != null) {
-            parents.add(mother);
+            getParents().add(mother);
         }
     }
 
@@ -46,27 +46,102 @@ public class Member {
 
     //есть родители (хотя бы один)
     public boolean hasParent() {
-        return !parents.isEmpty();
+        return !getParents().isEmpty();
     }
 
     //это его или её родитель
     public boolean isHisOrHerParent(Member parent) {
-        return parents.contains(parent);
+        return getParents().contains(parent);
     }
 
     //есть ребёнок
     public boolean hasChildren(Member child) {
-        return !children.isEmpty();
+        return !getChildren().isEmpty();
     }
 
     //это его или её ребёнок
     public boolean isHisOrHerChild(Member child) {
-        return children.contains(child);
+        return getChildren().contains(child);
     }
 
     //возраст
     public Integer getAge(LocalDate dateOfBirth, LocalDate dateOfDeath) {
         Period between = Period.between(dateOfBirth, dateOfDeath);
         return between.getYears();
+    }
+
+    @Override
+    public String toString(){
+        return "Member{" +
+                "name" + name +
+                ", surname"+ getSurname() +
+                ", sex" + getSex() +
+                ", dateOfBirth" + getDateOfBirth() +
+                ", dateOfDeath" + getDateOfDeath() +
+                ", parents = (" + getParents() + ")" +
+                ", children = (" + getChildren() + ")" +
+                "}";
+    }
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+    public Integer getId(){
+        return this.id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    };
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    public List<Member> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Member> children) {
+        this.children = children;
+    }
+
+    public List<Member> getParents() {
+        return parents;
+    }
+
+    public void setParents(List<Member> parents) {
+        this.parents = parents;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public LocalDate getDateOfDeath() {
+        return dateOfDeath;
+    }
+
+    public void setDateOfDeath(LocalDate dateOfDeath) {
+        this.dateOfDeath = dateOfDeath;
     }
 }
